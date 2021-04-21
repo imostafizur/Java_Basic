8ce6062e43bfede6835472585f9659a63add2b58
imostafizur <umostafizurs@gmail.com>
2021-04-21 15:33:08 +0600

    Increment example

---
+++ Increment/src/Increment.java
@@ -0,0 +1,14 @@
+
+public class Increment {
+public static void main(String[] args) {
+	int a = 1 ,b =1 ,c=1,d=1;
+	
+	a++ ; // postincrement
+	b-- ; // postdecrement
+	++c ; // preincrement
+	--d ; // postdecrement
+	
+	
+	System.out.println(+a+" "+b+" "+c+" "+d);
+}
+}
